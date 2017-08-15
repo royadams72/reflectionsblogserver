ba04be6083202b6a13f78113bfc7ed16c4e0c542
Roy Adams <royadams@Roys-iMac.local>
2017-08-15 22:12:31 +0100

    changed url

--- routes/blogs.js
+++ routes/blogs.js
@@ -4,7 +4,7 @@ var Blog = require('../models/blog');
 
 //route: middleware applied to a request (middleware acts as a bridge between application and database)
 //With Nodejs http methods; get post, patch delete
-router.get('/blogs', function (req, res, next) {
+router.get('/', function (req, res, next) {
   //Create an new instance of User object
     // var user = new User({firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, password: bcrypt.hashSync(req.body.password, 10)});
     Blog.find()
