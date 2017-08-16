bb67ff4e899632569d81b33a9efe96ffadb6857f
Roy Adams <royadams@Roys-iMac.local>
2017-08-16 19:57:46 +0100

    updated

--- routes/blogs.js
+++ routes/blogs.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res, next) {
   //Create an new instance of User object
     // var user = new User({firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, password: bcrypt.hashSync(req.body.password, 10)});
     Blog.find()
-    .exec(function(err, blogs){
+    .exec(function(err, response){
       if(err){
         return res.status(500).json({
           title: 'An error has occured',
@@ -18,7 +18,7 @@ router.get('/', function (req, res, next) {
     //console.log(messages)
       res.status(201).json({
         message: 'Success',
-        obj: blogs
+        blogs: response
       })
     });
 });
