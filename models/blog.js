1d9d7aa9d331ec389eedb5cf53ad93c8f59eef7b
Roy Adams <royadams@roys-mbp.home>
2017-08-19 14:46:47 +0100

    adding post

--- models/blog.js
+++ models/blog.js
@@ -7,8 +7,7 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
   title: {type: String, required:true},
   date: {type: String, required:true},
-  vidUrl: {type: String, required:true},
-  script: {type: String, required:true}
+  vidUrl: {type: String, required:true}
 });
 //export schema
 module.exports = mongoose.model('Blog', schema);//Will export as message
