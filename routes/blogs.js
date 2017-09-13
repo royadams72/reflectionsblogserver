6af0fa90467169a7bc0ece0cf8e420399d85ad7b
Roy Adams <royadams@Roys-iMac.local>
2017-09-13 19:33:41 +0100

    updating

--- routes/blogs.js
+++ routes/blogs.js
@@ -23,6 +23,26 @@ router.get('/', function (req, res, next) {
     });
 });
 
+router.get('/:id', function (req, res, next) {
+  //Create an new instance of User object
+    // var user = new User({firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, password: bcrypt.hashSync(req.body.password, 10)});
+    Blog.find(function(err, response){
+      if(err){
+        return res.status(500).json({
+          title: 'An error has occured',
+          error: err
+        })
+      }
+    //console.log(messages)
+      res.status(201).json({
+        message: 'Success',
+        blog: response
+      })
+    })
+
+});
+
+
 router.post('/', function (req, res, next) {
   //Create an new instance of User object
     // var user = new User({firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, password: bcrypt.hashSync(req.body.password, 10)});
